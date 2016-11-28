3e4a1dc1e20e423bfc89c886494b7a07ed4c6404
Lorenzo Toso <LorToso@gmail.com>
2016-11-28 15:59:05 +0100

    Started work on reformatting the address data

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Lorenzo Toso on 28.11.2016.
@@ -32,19 +36,30 @@ public class Main {
     }
 
     private static CSVRecord reformat(CSVRecord record) throws IOException {
-
-
         Map<String, String > recordAsMap = record.toMap();
         Map<String, String > cleanRecordAsMap = record.toMap();
 
         cleanNonNumericValues(recordAsMap, cleanRecordAsMap);
-        cleanZipCode(recordAsMap, cleanRecordAsMap);
+        cleanAddressData(recordAsMap, cleanRecordAsMap);
 
         return generateNewRecord(cleanRecordAsMap, record);
     }
 
-    private static void cleanZipCode(Map<String, String> recordAsMap, Map<String, String> cleanRecordAsMap) throws IOException {
-        Document doc = Jsoup.connect("https://tools.usps.com/go/ZipLookupAction!input.action").get();
+    private static void cleanAddressData(Map<String, String> recordAsMap, Map<String, String> cleanRecordAsMap) throws IOException {
+
+        String city = recordAsMap.get("City");
+        String state = recordAsMap.get("State");
+
+
+        String URI = "https://tools.usps.com/go/ZipLookupResultsAction!input.action?resultMode=1&companyName=&address1=&address2=&city=" + city + "&state=" + state + "&urbanCode=&postalCode=&zip=";
+
+        Document doc = Jsoup.connect(URI).get();
+
+        Elements allZips = doc.select(".zip");
+        List<String> zips = allZips.stream().map(Element::data).collect(Collectors.toList());
+
+
+
     }
 
     private static void cleanNonNumericValues(Map<String, String> recordAsMap, Map<String, String> cleanRecordAsMap) {
