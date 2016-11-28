d954533373f29adaf8ce0dcbc9aa99f27918f205
Lorenzo Toso <LorToso@gmail.com>
2016-11-28 15:16:54 +0100

    Records are now capitalized if necessary

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.StringUtils;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Map;
 
 /**
  * Created by Lorenzo Toso on 28.11.2016.
@@ -14,17 +15,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Reader in = new FileReader(args[0]);
 
-        CSVParser parser = new CSVParser(in , CSVFormat.MYSQL);
+        CSVParser parser = new CSVParser(in , CSVFormat.MYSQL.withDelimiter(',').withHeader());
         for (CSVRecord record : parser) {
             CSVRecord newRecord = reformat(record);
         }
+
     }
 
     private static CSVRecord reformat(CSVRecord record) {
-        for(String val : record)
+
+
+        Map<String, String > recordAsMap = record.toMap();
+        Map<String, String > cleanRecordAsMap = record.toMap();
+
+        for(Map.Entry<String,String> tuple : recordAsMap.entrySet())
         {
-            System.out.println(val);
+            String value = tuple.getValue();
+            String key = tuple.getKey();
+
+            value = capitalizeIfNecessary(key, value);
+
+            cleanRecordAsMap.put(key, value);
         }
-        return record;
+
+        return generateNewRecord(cleanRecordAsMap, record);
+    }
+
+    private static String capitalizeIfNecessary(String key, String value) {
+
+        if(key.equals("DOB"))
+            return value;
+        if(key.equals("PO BOX"))
+            return value;
+        if(key.equals("POCityStateZi"))
+            return value;
+
+        if(!StringUtils.isNumeric(value))
+        {
+            return StringUtils.capitalize(value);
+        }
+        return value;
+
+    }
+
+    private static CSVRecord generateNewRecord(Map<String, String> cleanRecordAsMap, CSVRecord record) {
+        return null;
     }
 }
