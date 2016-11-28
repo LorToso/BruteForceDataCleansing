2f573a44cb7b1ca97e8e70d054a07d77eacfd49c
Lorenzo Toso <LorToso@gmail.com>
2016-11-28 21:22:41 +0100

    Added getZip-Method

--- src/PlaceChunk.java
+++ src/PlaceChunk.java
@@ -20,11 +20,17 @@ public class PlaceChunk {
 
     public void lookupMissingFields()
     {
+        cleanZipCodes();
         // Clean Zip
         // Generate City&State from ZIP
         // Generate ZIP from City&State&Address (also formats address)
 
     }
+
+    private void cleanZipCodes() {
+
+    }
+
     public List<Place> getPlaces()
     {
         return places;
@@ -76,6 +82,36 @@ public class PlaceChunk {
             places.add(p);
         }
 
+        return places;
+    }
+
+    private static List<Place> getZip(Place[] address) throws IOException {
+        String url = "http://production.shippingapis.com/ShippingAPI.dll?API=ZipCodeLookup&XML=";
+        StringBuilder finalRequest = new StringBuilder();
+        finalRequest.append(url);
+        finalRequest.append("<ZipCodeLookupRequest USERID=\"" + UserID + "\">");
+        for (int i = 0; i < address.length; i++) {
+            finalRequest
+                    .append("<Address ID=\'" + i + "\'>")
+                    .append("<FirmName></FirmName>")
+                    .append("<Address1></Address1>")
+                    .append("<Address2>" + address[i].address + "</Address2>")
+                    .append("<City>" + address[i].city + "</City>")
+                    .append("<State>" + address[i].state + "</State>")
+                    .append("</Address>");
+        }
+        finalRequest.append("<ZipCodeLookupRequest USERID=\"" + UserID + "\">");
+        Document doc = Jsoup.connect(finalRequest.toString()).get();
+        Elements body = doc.select("Address");
+        List<Place> places = new ArrayList<>();
+        for (Element element : body) {
+            Place p = new Place();
+            p.zip = element.select("Zip5").text();
+            p.city = element.select("city").text();
+            p.state = element.select("state").text();
+            p.address = element.select("Address2").text();
+            places.add(p);
+        }
         return places;
     }
 }
